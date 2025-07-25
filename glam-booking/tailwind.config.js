a6ad1c0166d652901a72896a4d9eec82b677b6ae
lazoryks <lazoriksua@gmail.com>
2025-07-26 00:10:48 +0100

    tailwind

--- glam-booking/tailwind.config.js
+++ glam-booking/tailwind.config.js
@@ -8,3 +8,7 @@ module.exports = {
   },
   plugins: [],
 }
+// This is a basic Tailwind CSS configuration file that specifies the content paths
+// where Tailwind should look for class names to generate styles. It also extends
+// the default theme, allowing for custom styles to be added later. The plugins array
+// is currently empty, but can be used to include additional Tailwind CSS plugins in the future.
